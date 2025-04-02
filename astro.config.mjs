68364e7af79bf6e495158b1b8b907abb017de2b6
RenatoVA <renato.vivas2001@gmail.com>
2025-04-02 18:57:22 -0500

    Fix Tailwind CSS configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
   site: 'https://claudiavivas.github.io',
   base: '/cuine',
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
